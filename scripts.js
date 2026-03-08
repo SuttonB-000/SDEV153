4c32d641ac0008594f6991ec555bc66e1841fb3a
Suttonb-000 <Suttonb-000@protonmail.com>
2026-03-08 18:54:45 -0400

    Final big push to recitify and refactor, clean and ready for submission

--- scripts.js
+++ scripts.js
@@ -1,20 +1,37 @@
-// when the user clicks on the button toggle between hiding and showing content 
-function dropDownMenu() {
-    document.getElementById("myDropDown").classList.toggle("show");
+// // when the user clicks on the button toggle between hiding and showing content 
+// function dropDownMenu() {
+//     document.getElementById("myDropDown").classList.toggle("show");
+// }
+
+
+
+// // close the drop down menu if the user clicks outside of it 
+// window.onclick = function(event) {
+//     if (!event.target.matches('.menuIcon')) {
+//         var dropDowns = this.document.getElementsByClassName("menuContent");
+//         var i;
+//         for (i=0; i < dropDowns.length; i++) {
+//             var openDropDown = dropDowns[i];
+//             if (openDropDown.classList.contains("show")){
+//                 openDropDown.classList.remove("show");
+//             }
+//         }
+//     }
+// }
+
+// toggle the mobile menu
+function toggleMenu() {
+    const menu = document.getElementById('myDropDown');
+    menu.classList.toggle('show');
 }
 
-
-
-// close the drop down menu if the user clicks outside of it 
-window.onclick = function(event) {
-    if (!event.target.matches('.menuIcon')) {
-        var dropDowns = this.document.getElementsByClassName("menuContent");
-        var i;
-        for (i=0; i < dropDowns.length; i++) {
-            var openDropDown = dropDowns[i];
-            if (openDropDown.classList.contains("show")){
-                openDropDown.classList.remove("show");
-            }
-        }
+// close the menu when clicking outside
+window.addEventListener('click', function(event) {
+    const menu = document.getElementById('myDropDown');
+    const button = document.querySelector('.menuIcon');
+    
+    // if click is NOT on the button or inside the menu, close it
+    if (!menu.contains(event.target) && !button.contains(event.target)) {
+        menu.classList.remove('show');
     }
-}
+});
